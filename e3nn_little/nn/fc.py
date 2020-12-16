eaaf7cc4f8a39ba6eb46578d927ff41671cb8567
Mario Geiger <geiger.mario@gmail.com>
2020-12-16 19:11:59 +0100

    FC

--- e3nn_little/nn/fc.py
+++ e3nn_little/nn/fc.py
@@ -5,7 +5,7 @@ from e3nn_little.math import normalize2mom
 
 
 class FC(torch.nn.Module):
-    def __init__(self, hs, act):
+    def __init__(self, hs, act, in_scale='norm', out_scale='zero', out_act=False):
         super().__init__()
         assert isinstance(hs, tuple)
         self.hs = hs
@@ -16,6 +16,11 @@ class FC(torch.nn.Module):
 
         self.weights = torch.nn.ParameterList(weights)
         self.act = normalize2mom(act)
+        self.in_scale = in_scale
+        self.out_scale = out_scale
+        self.out_act = out_act
+        assert self.in_scale in ['component', 'norm']
+        assert self.out_scale in ['component', 'zero']
 
     def __repr__(self):
         return f"{self.__class__.__name__}{self.hs}"
@@ -23,47 +28,25 @@ class FC(torch.nn.Module):
     def forward(self, x):
         with torch.autograd.profiler.record_function(repr(self)):
             for i, W in enumerate(self.weights):
+                if i == 0:  # first layer
+                    if self.in_scale == 'component':
+                        W = W / x.shape[1]**0.5
                 if i > 0:  # not first layer
                     W = W / x.shape[1]**0.5
                 if i == len(self.weights) - 1:  # last layer
-                    W = W / x.shape[1]**0.5
+                    if self.out_scale == 'zero' and not self.out_act:
+                        W = W / x.shape[1]**0.5
 
                 x = x @ W
 
-                # not last layer
-                if i < len(self.weights) - 1:
+                if i < len(self.weights) - 1:  # not last layer
+                    x = self.act(x)
+                if i == len(self.weights) - 1 and self.out_act:  # last layer
                     x = self.act(x)
 
-            return x
-
-
-class FCrelu(torch.nn.Module):
-    def __init__(self, hs):
-        super().__init__()
-        assert isinstance(hs, tuple)
-        self.hs = hs
-        weights = []
-
-        for h1, h2 in zip(self.hs, self.hs[1:]):
-            weights.append(torch.nn.Parameter(torch.randn(h1, h2)))
-
-        self.weights = torch.nn.ParameterList(weights)
-
-    def __repr__(self):
-        return f"{self.__class__.__name__}{self.hs}"
-
-    def forward(self, x):
-        with torch.autograd.profiler.record_function(repr(self)):
-            for i, W in enumerate(self.weights):
-                if i > 0:  # not first layer
-                    W = 2**0.5 * W / x.shape[1]**0.5
                 if i == len(self.weights) - 1:  # last layer
-                    W = W / x.shape[1]**0.5
+                    if self.out_scale == 'zero' and self.out_act:
+                        x = x / W.shape[0]**0.5
 
-                x = x @ W
-
-                # not last layer
-                if i < len(self.weights) - 1:
-                    x.relu_()
 
             return x
